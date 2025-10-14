34d23e8b72fdec437af05a872d4a93d1fa152cb4
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-10-14 00:20:58 -0700

    Enhance CLI: Add server management actions and server types

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
+use std::{fmt::Display, path::PathBuf};
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -10,4 +10,38 @@ pub struct Args {
 }
 
 #[derive(Debug, Subcommand)]
-enum Action {}
+enum Action {
+    /// Create a new server
+    New {
+        /// The name of the new server
+        server: PathBuf,
+
+        /// The type of server software to use
+        #[arg(default_value_t = ServerTypes::Vanilla)]
+        kind: ServerTypes,
+
+        /// The Minecraft version to install
+        #[arg(default_value_t = String::from("latest"))]
+        mc_version: String,
+    },
+
+    /// Update existing server
+    Update {
+        /// The name of the server to update
+        server: PathBuf,
+    },
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum ServerTypes {
+    /// A vanilla server by Mojang
+    Vanilla,
+}
+
+impl Display for ServerTypes {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ServerTypes::Vanilla => write!(f, "vanilla"),
+        }
+    }
+}
